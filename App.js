3f0cb0ff1f28e290a23aa9e8211ef3c972e8766c
DiegxR <juandiegorojasarredondo@gmail.com>
2024-04-03 17:04:44 -0500

    add webpack config

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
 
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 export default function App() {
   return (
     <View  style={styles.container}>
-      <Text className='bg-white p-3 rounded-md text-black' >¡Arbol & Miel!</Text>
+      <Text className='bg-white p-3 rounded-md transition-all hover:text-[100px] cursor-pointer text-black' >¡Arbol & Miel!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,5 @@ module.exports = {
     extend: {},
   },
   plugins: ["nativewind/babel"],
-  presets: ["babel-preset-expo"]
 };
 
