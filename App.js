9dab61f0b2cdffce976c703002d2c5087ad078d7
DiegxR <juandiegorojasarredondo@gmail.com>
2024-04-05 20:34:05 -0500

    Create login view

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Login } from "./src/screens/Login";
+import HomeScreen from "./src/screens/Home";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -10,36 +12,30 @@ NativeWindStyleSheet.setOutput({
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+
+// function DetailsScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Details Screen</Text>
+//     </View>
+//   );
+// }
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "Inicia Sesión" }}
+        />
+        <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Overview" }}
         />
-        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
